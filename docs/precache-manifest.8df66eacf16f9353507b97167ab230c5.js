11e05b1ac91726c0052255346d132007288411c2
Orie Steele <orie@transmute.industries>
2020-04-11 10:34:47 -0500

    :rocket:

---
+++ docs/precache-manifest.8df66eacf16f9353507b97167ab230c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2637cc0f16ef0678b76cb3293c72fb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65412449d7c526bc89fb",
+    "url": "/static/css/main.9dee2d04.chunk.css"
+  },
+  {
+    "revision": "985d5521ff2865174b43",
+    "url": "/static/js/2.ffca3173.chunk.js"
+  },
+  {
+    "revision": "0b16e56dd20d3e04fb55b841e9c71cb5",
+    "url": "/static/js/2.ffca3173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65412449d7c526bc89fb",
+    "url": "/static/js/main.ceee9fed.chunk.js"
+  },
+  {
+    "revision": "855e11a27d56b351a88f",
+    "url": "/static/js/runtime-main.044f8e90.js"
+  },
+  {
+    "revision": "33b194d8ce06cb2e6794ecb444201773",
+    "url": "/static/media/logo.33b194d8.svg"
+  }
+]);
